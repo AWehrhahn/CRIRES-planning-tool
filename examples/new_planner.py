d7bf0359fdc874513b4ed194e2b16a40d001667e
Ansgar Wehrhahn <31626864+AWehrhahn@users.noreply.github.com>
2021-07-06 13:24:16 +0200

    add  plotting to script

--- examples/new_planner.py
+++ examples/new_planner.py
@@ -2,6 +2,7 @@ from astropy.time import Time
 from astropy import units as u
 
 from crires_planning_tool.transit_planner import transit_calculation
+from crires_planning_tool.interactive_graph import create_interactive_graph
 
 planet = "TRAPPIST-1"
 date_start = Time.now()
@@ -9,6 +10,8 @@ date_end = date_start + 1000 * u.day
 
 df = transit_calculation(planet, date_start, date_end)
 df.to_csv("test.csv")
+
+create_interactive_graph(df, "plot1.html", "plot2.html")
 
 pass
 
