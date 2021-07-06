a867d1f4c8ee69da2679d4e442ec24a8d0a2696a
Ansgar Wehrhahn <31626864+AWehrhahn@users.noreply.github.com>
2021-07-06 13:07:12 +0200

    caching for the database

--- crires_planning_tool/nasa_exoplanets_archive.py
+++ crires_planning_tool/nasa_exoplanets_archive.py
@@ -2,8 +2,11 @@ from astroquery.utils.tap.core import TapPlus
 from astroquery.nasa_exoplanet_archive import (
     NasaExoplanetArchive as _NasaExoplanetArchive,
 )
-
-
+from astropy.utils.data import clear_download_cache, import_file_to_cache, download_file
+from tempfile import NamedTemporaryFile
+from astropy.table import Table
+import urllib
+import json
 class NasaExoplanetsArchive:
     def __init__(self, timeout=10, verbose=0):
         #:int: the number of tries before timing out the query
@@ -40,6 +43,55 @@ class NasaExoplanetsArchive:
     def archive(self, value):
         raise AttributeError("Set the archive via the url property")
 
+    def reset_cache(self):
+        clear_download_cache(pkgname="crires-planning-tool")
+
+    def get_filename_from_cache(self, filename):
+        try:
+            url = "file:" + urllib.parse.quote(filename)
+            fname = download_file(url, cache=True, pkgname="crires-planning-tool", show_progress=False)
+        except:
+            fname = None
+        return fname
+
+    def import_file_to_cache(self, key, filename):
+        url = "file:" + urllib.parse.quote(key)
+        import_file_to_cache(url, filename, pkgname="crires-planning-tool", replace=True)
+
+
+    def from_cache_table(self, asql_query):
+        try:
+            fname = self.get_filename_from_cache(asql_query)
+            data = Table.read(fname, format="pandas.csv")
+        except (KeyError, urllib.error.URLError):
+            data = None
+        except:
+            data = None
+        return data
+
+    def to_cache_table(self, asql_query, data):
+        with NamedTemporaryFile("w", suffix=".csv") as tempfile:
+            data.write(tempfile, format="pandas.csv")
+            tempfile.flush()
+            self.import_file_to_cache(asql_query, tempfile.name)
+
+    def from_cache_json(self, fname):
+        fname = self.get_filename_from_cache("aliases.json")
+        try:
+            with open(fname) as f:
+                data = json.load(f)
+        except:
+            data = None
+        return data
+
+    def to_cache_json(self, fname, data):
+        with NamedTemporaryFile("w", suffix=".json") as tempfile:
+            json.dump(data, tempfile)
+            tempfile.flush()
+            self.import_file_to_cache(fname, tempfile.name)
+
+
+
     def tap(self, asql_query):
         """Get data from the TAP interface
 
@@ -60,11 +112,17 @@ class NasaExoplanetsArchive:
         RuntimeError
             Could not get results with the given query
         """
-        data = None
+        
+        data = self.from_cache_table(asql_query)
+        if data is not None:
+            if self.verbose >= 1:
+                print("Retrieving catalog data from local cache")
+            return data
+
         for i in range(self.timeout):
             try:
                 # data = NasaExoplanetArchive.query_object(name)
-                query = self.archive.launch_job(asql_query)
+                query = self.archive.launch_job(asql_query, verbose=self.verbose>=1)
                 data = query.results
                 break
             except ConnectionError:
@@ -77,6 +135,9 @@ class NasaExoplanetsArchive:
         elif data is None:
             raise RuntimeError(f"Invalid query: {asql_query}")
 
+        # Store it in the cache for later
+        self.to_cache_table(asql_query, data)
+      
         return data
 
     def query_object(self, name, regularize=True):
@@ -98,28 +159,59 @@ class NasaExoplanetsArchive:
         return self.query_object([name], regularize=regularize)
 
     def query_objects(self, names, regularize=True):
-        aliases = {}
-        if regularize:
-            for i, name in enumerate(names):
-                alias = _NasaExoplanetArchive._regularize_object_name(name)
-                aliases[alias] = name
-                names[i] = alias
-        else:
-            aliases = {n:n for n in names}
+        """
+        Request information for a set of planets or stars
 
-        asql_query = f"SELECT top 100 * FROM {self.table} WHERE "
+        Parameters
+        ----------
+        names : list
+            list of planet names
+        regularize : bool, optional
+            whether to check the database for the correct names, by default True.
+            This creates overhead but is more convenient.
+
+        Returns
+        -------
+        [type]
+            [description]
+        """
+        original = {}
+        if regularize:
+            aliases = self.from_cache_json("aliases.json")
+            aliases = {} if aliases is None else aliases
+            hasChanged = False
+            for i, name in enumerate(names):
+                if name in aliases.keys():
+                    alias = aliases[name]
+                else:
+                    alias = _NasaExoplanetArchive._regularize_object_name(name)
+                    hasChanged = True
+                    aliases[name] = alias
+                original[alias] = name
+                names[i] = alias
+            if hasChanged:
+                self.to_cache_json("aliases.json", aliases)
+
+        asql_query = f"SELECT * FROM {self.table} WHERE "
         asql_query += " OR ".join([f"pl_name like '%{name}%'" for name in names])
 
         data = self.tap(asql_query)
 
-        for i, d in enumerate(data):
-            try:
-                name = d["pl_name"]
-                name = aliases[name]
-            except KeyError:
-                name = d["hostname"]
-                name = aliases.get(name, name)
-                name = f"{name} {d['pl_letter']}"
-            data[i]["pl_name"] = name
+        if regularize:
+            for i, d in enumerate(data):
+                try:
+                    name = d["pl_name"]
+                    name = original[name]
+                except KeyError:
+                    name = d["hostname"]
+                    name = original.get(name, name)
+                    name = f"{name} {d['pl_letter']}"
+                data[i]["pl_name"] = name
 
+        return data
+
+    def query_criteria(self, criteria):
+        asql_query = f"SELECT * FROM {self.table} WHERE "
+        asql_query += " AND ".join(criteria)
+        data = self.tap(asql_query)
         return data
